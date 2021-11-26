0334fb332215ea7c8531b27fdcae5e1f6c8d167f
Soham677 <76729019+Soham677@users.noreply.github.com>
2021-11-26 10:49:21 +0530

    test

---
+++ config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDYNIFVoYIShkDQ_3HEgeFhsolI3795ieI",
+    authDomain: "fact-website.firebaseapp.com",
+    databaseURL: "https://fact-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fact-website",
+    storageBucket: "fact-website.appspot.com",
+    messagingSenderId: "173313139104",
+    appId: "1:173313139104:web:24440cb058ad0fbacfc4c5"
+  };
+
+  firebase.initializeApp(config);
